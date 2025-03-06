f07bfa846b36959b4842745525c82759dcf605d4
AutisticLulu <discord.meguminbot@gmail.com>
2025-03-06 12:53:38 +0100

    Re-structure and add third party addons that are recommended to use with MUI

--- MUI_Core/Engine/meta.lua
+++
@@ -1,8 +0,0 @@
----@meta
-
----@class _G
----@field MayronUI MayronUI
----@field GameTooltip GameTooltip
----@field BuffFrame Frame
----@field TemporaryEnchantFrame Frame
----@field DebuffFrame Frame
